a5e5eb378c50621ebc0cb3a5abe9815bd1adcb50
Nayem Alam <nayem.alam@mail.mcgill.ca>
2018-09-19 22:41:34 -0400

    assembly dot product working

--- GN_Lab1/asm/asm_dot.s
+++ GN_Lab1/asm/asm_dot.s
@@ -5,24 +5,28 @@ asm_dot
 
 ;**For ref: r0 = a, r1 = b, r2 = N, r3 = output result; as outlined in header file;
 
-			PUSH {R4,R5}                    ; saving context according to calling convention
-	
-			MOV 		 R6, #0                 ; set result = 0
-			VMOV.f32 S0, R6               	; floating point result
+			PUSH {R4-R6}                    ; saving context according to calling convention
 			
-LOOP	
-			SUBS 		 R2, R2, #1 						;counter 
-			BLT 		 DONE
-			ADD 		 R4, R0, R2, LSL #2 		;array1
-			ADD			 R5, R1, R2, LSL #2 		;array2
-			VLDR.f32 S1, [R4] 							
-			VLDR.f32 S2, [R5]
-			VMUL.f32 S2, S1, S2 
-			VADD.f32 S0, S0, S2 						;essentially: result+=a[i]*b[i]
+			VLDR.f32 	 S0, [R0]
+			VLDR.f32 	 S1, [R1]
+			MOV 		 R5, #0
+			VMOV.f32 	 S2, R5               	; floating point result
+			MOV 		 R6, #0                 ; set i = 0
+			
+LOOP					
+			ADD			 R6, R6, #1			;i
+			CMP 		 R2, R6				;N & i
+			BLT			 DONE				;when i < N -> DONE
+			ADD 		 R4, R0, R6, LSL #2 		;array1
+			ADD			 R5, R1, R6, LSL #2 		;array2
+			VMLA.f32 	 S2, S0, S1
+			VLDR.f32 	 S0, [R4] 							
+			VLDR.f32 	 S1, [R5]
+			;VADD.f32 S2, S2, S2 						;essentially: result+=a[i]*b[i]
 			B 			 LOOP		
-			
+
 DONE	
-			VSTR.f32 S0,[R3]
-			POP {R4,R5}
-			BX LR
+			VSTR.f32	 S2,[R3]
+			POP 		 {R4-R6}
+			BX 			 LR
 			END
