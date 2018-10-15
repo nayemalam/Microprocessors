093432eb022664d4392f49f60a06d4b6950a6cea
Nayem Alam <nayem.alam@mail.mcgill.ca>
2018-10-15 19:01:57 -0400

    mdk

---
+++ GN_Lab5/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'G14_Lab5' 
+ * Target:  'G14_Lab5' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
